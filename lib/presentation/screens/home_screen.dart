83a0e8ed1d72b1b1b53ce5bf7109f50b57200e16
josemlegal <josemlegal97@gmail.com>
2023-09-10 19:49:20 -0400

    Setup project

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          // ListTile(
+          //   title: const Text("Cubits"),
+          //   subtitle: const Text("Cubit is a subset of BLoC"),
+          //   trailing: const Icon(Icons.arrow_forward_ios_rounded),
+          //   onTap: () {
+          //     context.push('/cubits');
+          //   },
+          // ),
+          ListTile(
+            title: const Text("BloC"),
+            subtitle: const Text("Business Logic Component"),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
+            onTap: () {
+              context.push('/counter-bloc');
+            },
+          ),
+          // const Padding(
+          //   padding: EdgeInsets.symmetric(horizontal: 10),
+          //   child: Divider(),
+          // ),
+          // ListTile(
+          //   title: const Text("Nuevo Usuario"),
+          //   subtitle: const Text("Registro de nuevo usuario"),
+          //   trailing: const Icon(Icons.arrow_forward_ios_rounded),
+          //   onTap: () {
+          //     context.push('/new-user');
+          //   },
+          // ),
+        ],
+      ),
+    );
+  }
+}
